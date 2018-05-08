45207dc7b07aa3c0517c1093a0f7ffe3a33ef2c3
the_z <the_z@znel.org>
2018-05-08 12:01:01 +0100

    Include resources in setup, detect tk/tcl

--- setup.py
+++ setup.py
@@ -1,28 +1,42 @@
-
 import sys
 from cx_Freeze import setup, Executable
+import os.path
 
-base = None
-if sys.platform == "win32":
-	base = "Win32GUI"
+PYTHON_INSTALL_DIR = os.path.dirname(os.path.dirname(os.__file__))
+os.environ['TCL_LIBRARY'] = os.path.join(PYTHON_INSTALL_DIR, 'tcl', 'tcl8.6')
+os.environ['TK_LIBRARY'] = os.path.join(PYTHON_INSTALL_DIR, 'tcl', 'tk8.6')
+
+include_files = [
+    ('res/album_art.png','res/album_art.png'),
+	('res/btn_next.png','res/btn_next.png'),
+	('res/btn_pause.png','res/btn_pause.png'),
+	('res/btn_play.png','res/btn_play.png'),
+	('res/btn_prev.png','res/btn_prev.png'),
+	('res/btn_rand.png','res/btn_rand.png'),
+	('config.ini','config.ini'),
+	os.path.join(PYTHON_INSTALL_DIR, 'DLLs', 'tk86t.dll'),
+	os.path.join(PYTHON_INSTALL_DIR, 'DLLs', 'tcl86t.dll')
+]
 
 executables = [
 	Executable(
         "FoobarMiniplayer.py", 
-        base=base,
+        base = "Win32GUI",
+		targetName="FoobarMiniplayer.exe",
         icon="icon.ico")
 	]
 
 options = {
 	'build_exe' : {
+		'packages': ["os","PIL","mutagen","win32com","win32gui","win32con","win32api","tkinter"],
 		'optimize': 2,
-		'silent': 1
+		'include_files': include_files
 		}
 	}
 setup(
 	options = options,
 	name = "Foobar Miniplayer",
-	version = "1.0",
+	version = "1.0.1",
 	description = "Small COM GUI for foobar2k",
 	executables = executables
 	)
